0315a0b9c45d56df0d097a023febae24877ed788
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-05-28 18:53:21 -0300

    feat: add new format to test echo

--- srcs/commands/echo.c
+++ srcs/commands/echo.c
@@ -12,8 +12,42 @@
 
 #include "../../includes/minishell.h"
 
+void	check_flag(const char *arg, int *flag)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		if (arg[i] == '-' && arg[i] == 'n')
+		{
+			*flag = 1;
+			break ;
+		}
+		i++;
+	}
+}
+
 int	echo_adapter(const char *arg)
 {
-	ft_printf("%s\n", arg);
+	int	i;
+	int	flag;
+
+	flag = 0;
+	check_flag(arg, &flag);
+	if (arg == NULL)
+		ft_printf("\n");
+	else
+	{
+		i = 0;
+		while (arg[i] != '\0')
+		{
+			if (arg[i] != '"')
+				ft_printf("%c", arg[i]);
+			i++;
+		}
+		if (flag == 0)
+			ft_printf("\n");
+	}
 	return (0);
 }
