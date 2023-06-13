4876ba1e4c9d60b2e8eefc3814f0a69cc4aef6af
Carlos Rocha <carlos.rocha01@gmail.com>
2023-06-13 12:52:36 +0000

    feat(Makefile): including valgrind rule in Make file

--- srcs/commands/unset.c
+++ srcs/commands/unset.c
@@ -6,36 +6,11 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 10:14:31 by caalbert          #+#    #+#             */
-/*   Updated: 2023/06/12 23:16:15 by caalbert         ###   ########.fr       */
+/*   Updated: 2023/06/13 02:37:19 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-//extern char	**environ;
-
-// int	ft_unsetenv(const char *name)
-// {
-// 	int	i;
-// 	int	len;
-
-// 	i = 0;
-// 	len = ft_strlen(name);
-// 	while (environ[i])
-// 	{
-// 		if (ft_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-// 		{
-// 			while (environ[i])
-// 			{
-// 				environ[i] = environ[i + 1];
-// 				i++;
-// 			}
-// 			return (0);
-// 		}
-// 		i++;
-// 	}
-// 	return (-1);
-// }
 
 int	unset_adapter(t_cmds *cmds)
 {
@@ -44,7 +19,7 @@ int	unset_adapter(t_cmds *cmds)
 	printf("⚠️ Need to create!!! ⚠️\n");
 	return (0);
 	*/
-	
+
 	if (getenv(cmds->input->cmd_args) != NULL)
 	{
 		unsetenv(cmds->input->cmd_args);
