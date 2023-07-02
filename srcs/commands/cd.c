67e231ba9854e36a2a2638ce037456a516a8b610
Carlos Rocha <carlos.rocha01@gmail.com>
2023-07-01 22:21:56 -0300

    feature (Change Diretory): implements the first version of [cd] commnand

--- srcs/commands/cd.c
+++ srcs/commands/cd.c
@@ -6,7 +6,7 @@
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 10:13:53 by caalbert          #+#    #+#             */
-/*   Updated: 2023/06/08 19:47:02 by caalbert         ###   ########.fr       */
+/*   Updated: 2023/07/01 22:12:19 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int	cd_adapter(t_cmds *cmds)
 {
-	(void) cmds;
-	printf("⚠️ Need to create!!! ⚠️\n");
+	const char	*dir;
+
+	if (cmds->input->cmd_args == NULL)
+	{
+		dir = getenv("HOME");
+		if (dir == NULL)
+		{
+			fprintf(stderr, "cd: HOME not set\n");
+			return (1);
+		}
+	}
+	else
+		dir = cmds->input->cmd_args;
+	if (chdir(cmds->input->cmd_args) != 0)
+	{
+		perror("cd");
+		return (1);
+	}
 	return (0);
 }
