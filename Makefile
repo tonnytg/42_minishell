3b714c34cabd26e5095d97758f99903d59f2e069
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-07-23 11:34:53 -0300

    feat: solve all problem identified in echo

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ FILES	= \
 			srcs/commands/pipes.c 					\
 			srcs/builtins/cd.c						\
 			srcs/builtins/echo.c					\
+			srcs/builtins/echo_utils.c				\
 			srcs/builtins/env.c						\
 			srcs/builtins/exit.c					\
 			srcs/builtins/export.c					\
--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -12,62 +12,83 @@
 
 #include "../../includes/minishell.h"
 
-void	print_env_var(char *word)
+int	count_words(char **words)
 {
-	char	*result;
+	int	i;
+	int	count;
 
-	result = getenv(word);
-	if (result != NULL)
-		ft_printf("%s ", result);
-}
-
-void	print_args(t_cmds *cmds, int index)
-{
-	int		i;
-	char	*word;
-	char	**args;
-
-	i = index;
-	args = ft_split(cmds->input->cmd_args, ' ');
-	while (args[i] != NULL)
+	i = 0;
+	count = 0;
+	while (words[i] != NULL)
 	{
-		if (args[i][0] == '$')
-		{
-			word = ft_strtok(args[i], "$", 0);
-			if (ft_strcmp(word, "?") == 0)
-				ft_printf("%d ", cmds->exit_code.code);
-			else
-				print_env_var(word);
-		}
-		else
-			ft_printf("%s ", args[i]);
+		if (ft_strcmp(words[i], "-n") != 0 && ft_strcmp(words[i], "-e") != 0)
+			count++;
 		i++;
 	}
-	free_arr(args);
+	return (count);
+}
+
+char	**filter_echo_words(char **words)
+{
+	char	**filtered_words;
+	int		i;
+	int		j;
+	int		count;
+
+	j = 0;
+	count = count_words(words);
+	filtered_words = ft_calloc(count + 1, sizeof(char *));
+	i = 0;
+	while (words[i] != NULL)
+	{
+		if (ft_strcmp(words[i], "-n") != 0 && ft_strcmp(words[i], "-e") != 0)
+		{
+			filtered_words[j] = words[i];
+			j++;
+		}
+		i++;
+	}
+	return (filtered_words);
+}
+
+char	**set_echo_settings(char **words, int *settings)
+{
+	int		i;
+	char	**filter_words;
+
+	i = 0;
+	*settings = 0;
+	while (words[i] != NULL)
+	{
+		if (ft_strcmp(words[i], "-n") == 0)
+			*settings = *settings + 1;
+		if (ft_strcmp(words[i], "-e") == 0)
+			*settings = *settings + 2;
+		i++;
+	}
+	filter_words = filter_echo_words(words);
+	return (filter_words);
 }
 
 int	echo_adapter(t_cmds *cmds)
 {
+	int		settings;
+	int		exit_code;
 	char	**words;
-	int		trigger;
-	int		i;
+	char	**filter_words;
 
-	trigger = 0;
-	i = 0;
-	if (cmds->input->cmd_args == NULL)
+	if (cmds->current->full_args == NULL)
 	{
-		ft_printf("\n");
+		printf("\n");
+		cmds->exit_code.code = 0;
 		return (0);
 	}
-	words = ft_split(cmds->input->cmd_args, ' ');
-	if (ft_strcmp(words[0], "-n") == 0)
-	{
-		i = 1;
-		trigger = 1;
-	}
-	print_args(cmds, i);
-	if (trigger == 0)
-		ft_printf("\n");
+	settings = 0;
+	words = ft_split(cmds->current->full_args, ' ');
+	filter_words = set_echo_settings(words, &settings);
+	exit_code = echo_exec_print(cmds, settings, filter_words);
+	cmds->exit_code.code = exit_code;
+	free(filter_words);
 	free_arr(words);
-	return (0);
+	return (exit_code);
 }
