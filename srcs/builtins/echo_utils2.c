52e64bae74c1a855c3c6cfac0686c22ed4c926c2
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-07-26 08:19:59 -0300

    fix: leak 1 quotes

--- srcs/builtins/echo_utils2.c
+++ srcs/builtins/echo_utils2.c
@@ -42,23 +42,29 @@ char	*parse_string_to_envs(char *str)
 	char	**temp_str;
 	char	**converted_str;
 	char	*value;
+	char	*temp;
 	int		i;
 	int		j;
 
 	temp_str = ft_split(str, ' ');
 	converted_str = ft_calloc(count_arr(temp_str) + 1, sizeof(char *));
+	temp = NULL;
 	i = 0;
 	j = 0;
 	while (temp_str[i] != NULL)
 	{
-		if (getenv(remove_string(temp_str[i], '$')) != NULL)
+		temp = remove_string(temp_str[i], '$');
+		if (getenv(temp) != NULL)
 			converted_str[j++] = ft_strdup(
-					getenv(remove_string(temp_str[i], '$')));
+					getenv(temp));
 		else
 			converted_str[j++] = ft_strdup(temp_str[i]);
 		i++;
+		free(temp);
 	}
 	value = concatenate_strings((const char **)converted_str);
+	free_arr(converted_str);
+	free_arr(temp_str);
 	return (value);
 }
 
@@ -77,6 +83,7 @@ int	echo_arg_with_quotes(t_cmds *cmds)
 		temp_arg = parse_string_to_envs(new_arg);
 		free(new_arg);
 		new_arg = temp_arg;
+		free(temp_arg);
 	}
 	if (new_arg == NULL)
 		return (1);
