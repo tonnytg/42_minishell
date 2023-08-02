f5b1a6fb3cfd8267a0442f08a95b421fd4eecb0f
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-08-01 21:54:53 -0300

    feat: convert export with env inception

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -41,17 +41,87 @@ void	save_envs(t_cmds *cmds, char **envs, char *key, char *key_and_value)
 	envs[i] = NULL;
 }
 
+char	*convert_values(const char *value)
+{
+	int i;
+	int	j;
+	int k;
+	int	trigger;
+	char word[256];
+	char **arr_str;
+	char **arr;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	trigger = 0;
+	arr_str = ft_calloc(256, sizeof(char *) + 3);
+	arr = ft_calloc(256, sizeof(char *) + 3);
+	while (value[i] != '\0')
+	{
+		if (value[i] == '$' && trigger == 0)
+		{
+			trigger = 1;
+			i++;
+		}
+		if (value[i] == '$' && trigger == 1)
+		{
+			word[j++] = '\0';
+			arr_str[k] = ft_strdup(word);
+			k++;
+			j = 0;
+			trigger = 1;
+			i++;
+		}
+		if (value[i] == ' ' || value[i] == ':')
+		{
+			trigger = 0;
+			i++;
+		}
+		if (trigger == 1)
+		{
+			if (value[i] != '$')
+			{
+				word[j] = value[i];
+				j++;
+			}
+		}
+		i++;
+	}
+	word[j + 1] = '\0';
+	arr_str[k] = ft_strdup(word);
+	k++;
+	arr_str[k] = NULL;
+	i = 0;
+	while (arr_str[i] != NULL)
+	{
+		printf("arr_str[%d]: %s\n", i, arr_str[i]);
+		arr[i] = get_env(arr_str[i]);
+		printf("arr[%d]: %s\n", i, arr[i]);
+		i++;
+	}
+	char *result = concatenate_strings(arr);
+	free_arr(arr_str);
+	printf("result: %s\n", result);
+	return(result);
+}
+
 int	set_env_var(t_cmds *cmds, char *key, char *value)
 {
 	char	**envs;
 	char	*new_key;
 	char	*key_and_value;
+	char 	*new_value;
 
+	printf("[set_env_var] value: %s\n", value);
+	new_value = convert_values(value);
+	printf("[set_env_var] new_value: %s\n", new_value);
 	if (cmds->envs == NULL && cmds->current->full_args == NULL)
 		return (1);
 	envs = ft_calloc(count_arr(cmds->envs) + 2, sizeof(char *));
 	new_key = ft_strjoin(key, "=");
 	key_and_value = ft_strjoin(new_key, value);
+	printf("[set_env_var] key_and_value: %s\n", key_and_value);
 	save_envs(cmds, envs, key, key_and_value);
 	free(new_key);
 	free(key_and_value);
