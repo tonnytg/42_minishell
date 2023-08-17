595d70ff4cb0b2a1af56a31ba30986620c8da574
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-08-17 12:22:27 -0300

    feat: change split for new parse space

--- srcs/parser/interpreter.c
+++ srcs/parser/interpreter.c
@@ -32,13 +32,36 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	return (substring);
 }
 
+int	check_quote_phrase(char *phrase)
+{
+	int	i = 0;
+
+	while (phrase[i] != '\0')
+	{
+		if (phrase[i] == '\"' || phrase[i] == '\'')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 void	parse_values_args(t_cmds *cmds)
 {
 	int		i;
 	char	**words;
 
 	i = 0;
-	words = ft_split(cmds->current->phrase, ' ');
+	printf("[parse_values_args] - phrase: %s\n", cmds->current->phrase);
+	int result = check_quote_phrase(cmds->current->phrase);
+	printf("[parse_values_args] - result: %d\n", result);
+	if (result == 1)
+	{
+		words = ft_split(cmds->current->phrase, '\0');
+	}
+	else
+	{
+		words = ft_split(cmds->current->phrase, ' ');
+	}
 	cmds->current->phrase_parsed = ft_calloc(sizeof(char *),
 			count_arr(words) + 1);
 	while (words[i] != NULL)
@@ -52,38 +75,78 @@ void	parse_values_args(t_cmds *cmds)
 				cmds->current->phrase_parsed[i] = get_env(cmds, words[i], 1);
 		}
 		else if (ft_strncmp(words[i], ".", 1) == 0)
+		{
 			cmds->current->phrase_parsed[i] = parse_to_folder(cmds, words[i]);
+			printf("1 - phrase_parsed: %s\n", cmds->current->phrase_parsed[i]);
+		}
 		else
+		{
 			cmds->current->phrase_parsed[i] = ft_strdup(words[i]);
+			printf("2 - phrase_parsed: %s\n", cmds->current->phrase_parsed[i]);
+		}
 		i++;
 	}
 	free_arr(words);
+
+	t_cmd_node *temp3;
+	printf("---\n[3 prepare_phrase]---\n");
+	temp3 = cmds->cmd_list;
+	while (temp3 != NULL)
+	{
+		printf("phrase: %s\n", temp3->phrase);
+		printf("type: %s\n", temp3->type);
+		temp3 = temp3->next;
+	}
+
 }
 
 void	prepare_phrase(t_cmds *cmds)
 {
-	char	**splited_phrase;
-	char	**temp;
-	char	*temp_str;
-	char	*temp_str1;
-	int		i;
+//	char	**splited_phrase;
+//	char	**temp;
+//	char	*temp_str;
+//	char	*temp_str1;
+//	int		i;
 
-	splited_phrase = ft_split(cmds->current->phrase, ' ');
-	temp = ft_calloc(sizeof(char *), count_arr(splited_phrase) + 1);
-	i = 0;
-	while (splited_phrase[i] != NULL)
+
+	t_cmd_node *temp1;
+	printf("---\n[1 prepare_phrase]---\n");
+	temp1 = cmds->cmd_list;
+	while (temp1 != NULL)
 	{
-		temp_str = remove_string(splited_phrase[i], '\"');
-		temp[i] = remove_string(temp_str, '\'');
-		free(temp_str);
-		i++;
+		printf("phrase: %s\n", temp1->phrase);
+		printf("type: %s\n", temp1->type);
+		temp1 = temp1->next;
 	}
-	temp_str1 = concatenate_strings(temp, 0);
-	free(cmds->current->phrase);
-	cmds->current->phrase = ft_strdup(temp_str1);
-	free(temp_str1);
-	free_arr(temp);
-	free_arr(splited_phrase);
+
+	printf("Current Phrase: '%s'\n", cmds->current->phrase);
+//	splited_phrase = ft_split(cmds->current->phrase, ' ');
+//	temp = ft_calloc(sizeof(char *), count_arr(splited_phrase) + 1);
+//	i = 0;
+//	while (splited_phrase[i] != NULL)
+//	{
+//		temp_str = remove_string(splited_phrase[i], '\"');
+//		temp[i] = remove_string(temp_str, '\'');
+//		free(temp_str);
+//		i++;
+//	}
+//	temp_str1 = concatenate_strings(temp, 0);
+//	free(cmds->current->phrase);
+//	cmds->current->phrase = ft_strdup(temp_str1);
+//	free(temp_str1);
+//	free_arr(temp);
+//	free_arr(splited_phrase);
+
+	t_cmd_node *temp2;
+	printf("---\n[2 prepare_phrase]---\n");
+	temp2 = cmds->cmd_list;
+	while (temp2 != NULL)
+	{
+		printf("phrase: %s\n", temp2->phrase);
+		printf("type: %s\n", temp2->type);
+		temp2 = temp2->next;
+	}
+
 }
 
 void	init_interpreter(t_cmds *cmds)
--- srcs/parser/token_analysis.c
+++ srcs/parser/token_analysis.c
@@ -99,16 +99,72 @@ int	token_analysis(t_cmds *cmds)
 
 	list_tokens = NULL;
 	data_copy = ft_strdup(cmds->input->datacpy);
-	token = ft_strtok(data_copy, " ", 1);
-	while (token != NULL)
+
+
+	int i = 0;
+	int j = 0;
+	int skip = 0;
+	int close_quote = 0;
+	char *new_word;
+	new_word = ft_calloc(1, sizeof(char));
+	while (data_copy[i] != '\0')
 	{
-		add_tk_node(&list_tokens, token, "undefined");
-		token = ft_strtok(NULL, " ", 0);
+		if ((data_copy[i] == '\'' || data_copy[i] == '\"') && close_quote == 0)
+		{
+			close_quote = 1;
+		}
+		else if ((data_copy[i] == '\'' || data_copy[i] == '\"') && close_quote == 1)
+		{
+			close_quote = 0;
+		}
+		if (data_copy[i] == ' ' && close_quote == 0)
+		{
+			printf("\n");
+			new_word[j] = '\0';
+			if (ft_strlen(new_word) > 0)
+			{
+				printf("[%ld]Nova Palavra: '%s'\n",ft_strlen(new_word), new_word);
+				token = ft_strdup(new_word);
+				add_tk_node(&list_tokens, token, "undefined");
+			}
+			j = 0;
+			skip = 1;
+		}
+		if (skip == 1)
+		{
+			skip = 0;
+		}
+		else
+		{
+			printf("%c", data_copy[i]);
+			if (data_copy[i + 1] == '\0')
+				printf("\n");
+			new_word[j] = data_copy[i];
+			j++;
+		}
+		i++;
 	}
+	new_word[j] = '\0';
+	printf("Ultima palavra: '%s'\n", new_word);
+	token = ft_strdup(new_word);
+	add_tk_node(&list_tokens, token, "undefined");
+	printf("\n");
+	printf("new_word: %s\n", new_word);
+
+
+
+
+
+//	token = ft_strtok(data_copy, " ", 1);
+//	while (token != NULL)
+//	{
+//		add_tk_node(&list_tokens, token, "undefined");
+//		token = ft_strtok(NULL, " ", 0);
+//	}
 	classify_tk_nodes(list_tokens);
 	concat_tk_nodes(cmds, list_tokens);
 	build_struct_to_exec(cmds, list_tokens);
-	free_tk_nodes(list_tokens);
+//	free_tk_nodes(list_tokens);
 	free(data_copy);
 	return (1);
 }
